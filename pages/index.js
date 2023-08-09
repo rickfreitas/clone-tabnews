306eb40cc11576c04d37e7e7cc82eb0924ae0a7d
Henrique Freitas <henrique.manoel@hotmail.com.br>
2023-08-09 09:28:27 -0300

    message

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+function Home() {
+    return <div>
+        <h1>Meu amor</h1>
+        <p>Me sinto muito orgulhoso de te ver vencendo suas batalhas todos os dias!</p>
+        <p>Obrigado por ser minha vida, e me fazer tão <b>feliz!</b></p>
+        <p><b><i>Te amo mil milhões!!!</i></b></p>
+        </div>
+}
+
+export default Home;
