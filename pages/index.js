76d42242341563be9d60e277c8b0b30d8a72c043
Henrique Manoel de Freitas Santos <henrique.manoel@hotmail.com.br>
2024-02-08 20:29:25 -0300

    my idea

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,6 @@
 function Home() {
     return <div>
-        <h1>Meu amor</h1>
-        <p>Me sinto muito orgulhoso de te ver vencendo suas batalhas todos os dias!</p>
-        <p>Obrigado por ser minha vida, e me fazer tão <b>feliz!</b></p>
-        <p><b><i>Te amo mil milhões!!!</i></b></p>
+        <h1>Prof Amigo</h1>
         </div>
 }
 
